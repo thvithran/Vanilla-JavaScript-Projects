2ee1cb93258b5d822744aed5b248948d8f948e67
Thvithran <73452153+thvithran@users.noreply.github.com>
2023-09-14 21:48:21 +0800

    Add files via upload

---
+++ Projects/Countdown 2/script.js
@@ -0,0 +1,31 @@
+const finaleDate = new Date("March 19, 2024 18:00:00").getTime();
+
+const timer = () =>{
+    const now = new Date().getTime();
+    let diff = finaleDate - now;
+    // Showing the alert when the counter time finishes.
+    if(diff < 0){
+        document.querySelector('.alert').style.display = 'block';
+        document.querySelector('.container').style.display = 'none';
+    }
+    
+    let days = Math.floor(diff / (1000*60*60*24));
+    let hours = Math.floor(diff % (1000*60*60*24) / (1000*60*60));
+    let minutes = Math.floor(diff % (1000*60*60)/ (1000*60));
+    let seconds = Math.floor(diff % (1000*60) / 1000);
+  // Adding the zeros.
+    days <= 99 ? days = `0${days}` : days;
+    days <= 9 ? days = `00${days}` : days;
+    hours <= 9 ? hours = `0${hours}` : hours;
+    minutes <= 9 ? minutes = `0${minutes}` : minutes;
+    seconds <= 9 ? seconds = `0${seconds}` : seconds;   
+
+    document.querySelector('#days').textContent = days;
+    document.querySelector('#hours').textContent = hours;
+    document.querySelector('#minutes').textContent = minutes;
+    document.querySelector('#seconds').textContent = seconds;
+
+}
+timer();
+// Calling the function every 1000 milliseconds.
+setInterval(timer,1000);
